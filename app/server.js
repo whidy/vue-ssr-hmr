8a7a9af111a962d7b9361eaf364913fd0d04d653
Whidy_Macbook <whidy@whidy.net>
2019-12-02 17:23:00 +0800

    add lru-cache

--- app/server.js
+++ app/server.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const LRU = require('lru-cache')
 const Koa = require('koa')
 const Router = require('koa-router')
 
@@ -14,11 +15,16 @@ const clientManifest = require('../dist/vue-ssr-client-manifest.json')
 
 const app = new Koa()
 const router = new Router()
+
 const renderer = createBundleRenderer(serverBundle, {
   runInNewContext: false,
   template,
   clientManifest,
   inject: false,
+  cache: new LRU({
+    max: 1000,
+    maxAge: 1000 * 60 * 15,
+  }),
 })
 
 app.use(require('koa-static')(path.join(__dirname, '../dist')))
